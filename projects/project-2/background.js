c8b8230b8784442af4de45edbb0082cc07a81371
mbelgrod <mbelgrod@risd.edu>
2024-03-01 10:46:18 -0500

    new

--- projects/project-2/background.js
+++ projects/project-2/background.js
@@ -1,4 +1,5 @@
 // background.js
+
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
@@ -7,10 +8,27 @@ chrome.action.onClicked.addListener((tab) => {
   });
   
   function injectIndex() {
+    // Load your font
+    const fontUrl = chrome.runtime.getURL('Save.ttf');
+    
+    // Apply the font styles
+    const style = document.createElement('style');
+    style.textContent = `
+    @font-face {
+        font-family: "Save";
+        src: url('Save.ttf') format('truetype');
+    }
+  
+      body {
+        font-family: 'Save', sans-serif;
+      }
+    `;
+    document.head.appendChild(style);
+  
+    // Create and append the iframe
     const indexHtml = document.createElement('iframe');
     indexHtml.src = chrome.runtime.getURL('index.html');
     indexHtml.style.cssText = `
-      position: fixed;
       top: 0;
       left: 0;
       width: 100%;

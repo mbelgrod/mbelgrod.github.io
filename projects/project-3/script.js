673c71319c6d314ef43bc8bf809ddb829ec52db6
mbelgrod <mbelgrod@risd.edu>
2024-03-07 14:50:06 -0500

    New

---
+++ projects/project-3/script.js
@@ -0,0 +1,55 @@
+document.querySelectorAll('.draggable').forEach(dragElement);
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    // Bring the clicked element to the top
+    var highestZIndex = findHighestZIndex();
+    elmnt.style.zIndex = highestZIndex + 1;
+
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when the mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
+  function findHighestZIndex() {
+    var elements = document.querySelectorAll('.draggable');
+    var highestZIndex = 0;
+
+    elements.forEach(function (element) {
+      var zIndex = parseInt(window.getComputedStyle(element).zIndex);
+      if (!isNaN(zIndex) && zIndex > highestZIndex) {
+        highestZIndex = zIndex;
+      }
+    });
+  }
+} 
